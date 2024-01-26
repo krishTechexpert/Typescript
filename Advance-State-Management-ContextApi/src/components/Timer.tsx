ee054aeb720b27ae0d05994b50b68c85f5197af3
krish <krishcode9@gmail.com>
2024-01-26 19:04:22 +0530

    timer App has finished project

--- Advance-State-Management-ContextApi/src/components/Timer.tsx
+++ Advance-State-Management-ContextApi/src/components/Timer.tsx
@@ -1,10 +1,54 @@
+import {useState,useEffect,useRef} from 'react';
 import Container from './UI/Container.tsx';
-import { Timer as Timerprops } from './store/Timer-Context.tsx';
+import { Timer as Timerprops, useTimersContext } from './store/Timer-Context.tsx';
+
+
+
 export default function Timer({name,duration}:Timerprops) {
+
+  const [remainingTime,setRemainingTime] = useState(duration * 1000);
+  const interval=useRef<number | null>(null);
+  const {isRunning}  = useTimersContext();
+  
+  if(remainingTime <=0 && interval.current){
+    clearInterval(interval.current)
+  }
+
+
+  useEffect(() => {
+    let timer:number;
+    if(isRunning){
+
+    timer=setInterval(() => {
+      setRemainingTime(prevTime => {
+        if(prevTime<=0){
+          return prevTime
+        }
+        return prevTime - 50
+      })
+    },50)
+    interval.current=timer;
+
+    }
+    else if(interval.current){
+
+      clearInterval(interval.current)
+    }
+
+     return () =>{ 
+      console.log('cleaun up timer')
+      clearInterval(timer)
+    
+    }
+  },[isRunning])
+
+  const formatedRemainTime = (remainingTime/1000).toFixed(2);
+
   return (
     <Container as="article">
       <h2>{name}</h2>
-      <p>{duration}</p>
+      <p><progress max={duration * 1000} value={remainingTime} /></p>
+      <p>time remain: {formatedRemainTime}</p>
     </Container>
   );
 }
